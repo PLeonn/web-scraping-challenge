a1083eef4cd23492d2e1473db004f14413bb0915
Pleon <patrickedwardleon@gmail.com>
2020-09-21 12:07:13 -0700

    Finished app.py

--- app.py
+++ app.py
@@ -5,3 +5,21 @@ import scrape_mars
 app = Flask(__name__)
 
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+@app.route('/')
+def index():
+    
+    mars_data = mongo.db.mars_data.find_one()
+    return render_template('index.html', mars_data=mars_data)
+
+@app.route('/scrape')
+def scrape():
+
+    mars_data = mongo.db.mars_data
+    data = scrape_mars.scrape()
+    mars_data.update({}, data, upsert=True)
+
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
